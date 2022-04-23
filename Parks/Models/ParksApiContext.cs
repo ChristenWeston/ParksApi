74295e22470a9be4214577a47a680ef428cd25e0
Christen Weston <paleeserecycle@gmail.com>
2022-04-23 12:09:31 -0700

    Updated seed data for park

--- Parks/Models/ParksApiContext.cs
+++ Parks/Models/ParksApiContext.cs
@@ -16,11 +16,13 @@ namespace ParksApi.Models
         {
           builder.Entity<Park>()
             .HasData(
-              new Park{ ParkId = 1, Name = "Etosha National Park", Size = 8600, City = "", State = "", Country = "Namibia" },
-              new Park{ ParkId = 2, Name = "Grand Canyon National Park", Size = 1904, City = "", State = "Arizona", Country = "USA" },
-              new Park{ ParkId = 3, Name = "Jim Corbett National Park", Size = 201, City = "", State = "Uttarakhand", Country = "India" },
-              new Park{ ParkId = 4, Name = "Kruger National Park", Size = 7512, City = "", State = "", Country = "South Africa" },
-              new Park{ ParkId = 5, Name = "Dolomiti Bellunesi National Park", Size = 1, City = "", State = "", Country = "Italy" }
+              new Park{ ParkId = 1, Name = "Etosha", Size = 8600, City = "", State = "", Country = "Namibia" },
+              new Park{ ParkId = 2, Name = "Grand Canyon", Size = 1904, City = "", State = "Arizona", Country = "USA" },
+              new Park{ ParkId = 3, Name = "Jim Corbett", Size = 201, City = "", State = "Uttarakhand", Country = "India" },
+              new Park{ ParkId = 4, Name = "Kruger", Size = 7512, City = "", State = "", Country = "South Africa" },
+              new Park{ ParkId = 5, Name = "Dolomiti Bellunesi", Size = 12, City = "", State = "", Country = "Italy" },
+              new Park{ ParkId = 6, Name = "Mount Kilimanjaro", Size = 652, City = "Moshi", State = "", Country = "Tanzania" },
+              new Park{ ParkId = 7, Name = "Serengeti", Size = 5200, City = "", State = "", Country = "Tanzania" }
              );
         }
     }
